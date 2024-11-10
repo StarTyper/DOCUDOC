2e7d729417e080ff0547a6439d1ee0be9afb8b86
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2024-11-10 19:25:30 +0100

    seeded Admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,15 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Clear existing users
+User.destroy_all
+
+# Create sample users
+User.create!(
+  [
+    { email: 'florian.sitte@live.de', password: 'password123', password_confirmation: 'password123', username: 'florian.sitte', firstname: 'Florian', lastname: 'Sitte', role: 'admin' }
+  ]
+)
+
+puts "Users seeded!"
